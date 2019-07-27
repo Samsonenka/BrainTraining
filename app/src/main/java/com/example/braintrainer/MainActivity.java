95cfa5d7eb067ae1f08c8d07d2539d2e7fd0b207
Pavel Samsonenka <pavel.samsonenka92@gmail.com>
2019-07-27 21:58:22 +0200

    Added method onClick

--- app/src/main/java/com/example/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/braintrainer/MainActivity.java
@@ -4,7 +4,10 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView textViewOp1;
     private TextView textViewOp2;
     private TextView textViewOp3;
+
+    private ArrayList<TextView> options = new ArrayList<>();
 
     private String question;
     private int rightAnswer;
@@ -35,12 +40,32 @@ public class MainActivity extends AppCompatActivity {
         textViewOp1 = findViewById(R.id.textViewOp1);
         textViewOp2 = findViewById(R.id.textViewOp2);
         textViewOp3 = findViewById(R.id.textViewOp3);
+
+        options.add(textViewOp0);
+        options.add(textViewOp1);
+        options.add(textViewOp2);
+        options.add(textViewOp3);
+
+        playNext();
+
+    }
+
+    private void playNext(){
+
+        generateQuestin();
+        for (int i = 0; i < options.size(); i++) {
+            if (i == rightAnswerPosition){
+                options.get(i).setText(Integer.toString(rightAnswer));
+            }else {
+                options.get(i).setText(Integer.toString(generateWrongAnswer()));
+            }
+        }
     }
 
     private void generateQuestin(){
 
-        int a = (int)(Math.random() * (max - max + 1) + min);
-        int b = (int)(Math.random() * (max - max + 1) + min);
+        int a = (int)(Math.random() * (max - min + 1) + min);
+        int b = (int)(Math.random() * (max - min + 1) + min);
         int mark = (int)(Math.random() * 2);
         isPositive = mark == 1;
 
@@ -51,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
             rightAnswer = a - b;
             question = String.format("%s - %s", a, b);
         }
-
+        textViewQuestin.setText(question);
         rightAnswerPosition = (int)(Math.random() * 4);
     }
 
@@ -64,5 +89,10 @@ public class MainActivity extends AppCompatActivity {
         }while (result == rightAnswer);
 
         return result;
+    }
+
+    public void onClickAnswer(View view) {
+
+        playNext();
     }
 }
